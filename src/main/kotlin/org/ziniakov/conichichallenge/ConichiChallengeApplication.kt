c1043fcfaaa95439f3903d6b1b595542b2a36c7e
George Zinyakov <gzinyakov@alfabank.ru>
2019-11-13 11:43:45 +0300

    first commit

---
+++ src/main/kotlin/org/ziniakov/conichichallenge/ConichiChallengeApplication.kt
@@ -0,0 +1,11 @@
+package org.ziniakov.conichichallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConichiChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConichiChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/org/ziniakov/conichichallenge/ConichiChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ziniakov.conichichallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConichiChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
